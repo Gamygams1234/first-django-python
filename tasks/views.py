e6515191e1f6d294e097f60bdafcaef2091795a3
Gamygams1234 <gamyburgos@gmail.com>
2020-06-12 22:33:35 -0700

    Checking priority and going over severside validation

--- tasks/views.py
+++ tasks/views.py
@@ -7,6 +7,8 @@ tasks = ['foo', 'bar', 'baz']
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
+    # adding some functionality with a serverside and client side check
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=15)
 
 
 def index(request):
@@ -17,6 +19,17 @@ def index(request):
 
 
 def add(request):
+    # if the form is valid, this is how we get data form the form
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+        else:  # we send back the existing data if it does not work
+            return render(request, "tasks/add.html", {
+                'form': form
+            })
+
     return render(request, "tasks/add.html", {
         'form': NewTaskForm()
     })
